953dfddb0e4f4de4acb4c4a2ca937177ea52f61d
adammertel <mertel.adam@gmail.com>
2018-02-04 21:41:47 +0100

    countries data update

--- prepare_countries_names.py
+++ prepare_countries_names.py
@@ -9,17 +9,18 @@ ids_to_remove = []
 for ci, country in enumerate(countries['features']):
     found = False
     for code in codes:
-        if code['Code'] == country['properties']['iso_a2']:
+        if code['Code'] == country['properties']['ISO_A2']:
             found = True
             country['properties']['admin'] = code['Name']
 
     if found == False:
-        ids_to_remove.append(ci)
+        country['properties']['admin'] = country['properties']['NAME']
+        #ids_to_remove.append(ci)
 
-ids_to_remove.reverse()
+#ids_to_remove.reverse()
 
-for id in ids_to_remove:
-    del countries['features'][id]
+#for id in ids_to_remove:
+#del countries['features'][id]
 
 with open('countries.geojson', 'w') as file:
     json.dump(countries, file)
